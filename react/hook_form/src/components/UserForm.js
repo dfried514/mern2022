494f04bf668d4c4b78bd215f155506d359eaec88
dfried514 <dfried514@gmail.com>
2022-07-11 09:39:20 -0700

    form with validation

--- react/hook_form/src/components/UserForm.js
+++ react/hook_form/src/components/UserForm.js
@@ -16,33 +16,37 @@ const UserForm = (props) => {
         setPassword1("");
         setPassword2("");
     };
-    
     return(
         <>
             <form onSubmit={ createUser }>
                 <div>
                     <label>First Name:  </label>
                     <input type="text" value={ firstName } onChange={ (e) => setFirstName(e.target.value) } />
+                    { firstName.length === 1 && <p>First Name must be at lease 2 characters</p> }
                 </div>
                 <br />
                 <div>
                     <label>Last Name:  </label>
                     <input type="text" value={ lastName } onChange={ (e) => setLastName(e.target.value) } />
+                    { lastName.length === 1 && <p>Last Name must be at lease 2 characters</p> }
                 </div>
                 <br />
                 <div>
                     <label>Email:  </label> 
                     <input type="text" value={ email } onChange={ (e) => setEmail(e.target.value) } />
+                    { (email.length && email.length < 5) ? (<p>Email must be at lease 5 characters</p>) : "" }
                 </div>
                 <br />
                 <div>
                     <label>Password:  </label>
                     <input type="password" autoComplete="password" value= { password1 } onChange={ (e) => setPassword1(e.target.value) } />
+                    { (password1.length && password1.length < 8) ? (<p>Password must be at lease 8 characters</p>) : "" }
                 </div>
                 <br />
                 <div>
                     <label>Confirm Password:  </label>
                     <input type="password" autoComplete="password" value= { password2 } onChange={ (e) => setPassword2(e.target.value) } />
+                    { (password2.length && password2 !== password1) ? (<p>Passwords must match</p>) : "" }
                 </div>
                 <br />
                 <input type="submit" value="Create User" />
